33271c76c50668f6dcaf43a3cbf5a6822daac1cc
Irtiza Chowdhury <153156568+ich-wtag@users.noreply.github.com>
2024-02-21 19:31:07 +0600

    refactor: file name updated the constants

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
-import { HOME, LOGIN, REGISTER } from './Models/contants';
+import { HOME, LOGIN, REGISTER } from './Models/constants';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
