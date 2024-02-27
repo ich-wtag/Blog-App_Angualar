d994e57022a48fb8d9cd10e6ffe915482f13170e
Irtiza Chowdhury <153156568+ich-wtag@users.noreply.github.com>
2024-02-27 09:30:23 +0600

    refactor : vs code settings updated

--- src/app/user-profile/blog-creation-form/blog-creation-form.component.ts
+++ src/app/user-profile/blog-creation-form/blog-creation-form.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-blog-creation-form',
   templateUrl: './blog-creation-form.component.html',
-  styleUrls: ['./blog-creation-form.component.scss']
+  styleUrls: ['./blog-creation-form.component.scss'],
 })
-export class BlogCreationFormComponent {
-
-}
+export class BlogCreationFormComponent {}
