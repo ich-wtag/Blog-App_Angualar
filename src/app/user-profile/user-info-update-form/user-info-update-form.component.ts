7b3ce47d50d1ab67ea3c1d0880062fa64367ddbe
Irtiza Chowdhury <153156568+ich-wtag@users.noreply.github.com>
2024-03-10 10:26:54 +0600

    feat : styling for user info update form are implemented

--- src/app/user-profile/user-info-update-form/user-info-update-form.component.ts
+++ src/app/user-profile/user-info-update-form/user-info-update-form.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { GetControlName, imageTypeCheck } from 'src/app/Models/commonFunctions';
+import { DUMMYUSERIMAGE } from 'src/app/Models/constants';
 
 @Component({
   selector: 'app-user-info-update-form',
@@ -6,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-info-update-form.component.scss'],
 })
 export class UserInfoUpdateFormComponent {
-  userImageSource: string = '../../../assets/images/user-image.png';
+  userImageSource: any = DUMMYUSERIMAGE;
+  profileImageFileName?: string;
+
+  getControlName = GetControlName;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  userInfoForm: FormGroup = this.formBuilder.group({
+    name: new FormControl(''),
+    subTitle: new FormControl(''),
+    about: new FormControl(''),
+    profileImage: new FormControl(''),
+  });
+
+  handleImageChange(event: any) {
+    if (event.target?.files.length) {
+      const file: File = event.target?.files[0];
+      if (imageTypeCheck(file.name)) {
+        this.profileImageFileName = file.name;
+        const fileReader: FileReader = new FileReader();
+
+        fileReader.onload = (event) => {
+          this.userInfoForm.patchValue({
+            profileImage: event.target?.result,
+          });
+          this.userImageSource = event.target?.result;
+        };
+
+        fileReader.readAsDataURL(file);
+      }
+    }
+  }
+
+  onSubmnitUserInfo() {
+    console.log(this.userInfoForm.value);
+  }
 }
