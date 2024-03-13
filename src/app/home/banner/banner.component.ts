a4e368c67db6bdd2cf34a9a144999982e579f72d
Irtiza Chowdhury <153156568+ich-wtag@users.noreply.github.com>
2024-03-13 15:55:38 +0600

    style: home banner styled more correctly for logged in and logged out condition

--- src/app/home/banner/banner.component.ts
+++ src/app/home/banner/banner.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Blog } from 'src/app/Models/blog';
+import {
+  DUMMY_USER_IMAGE,
+  DUMMY_USER_IMAGE_LIGHT,
+} from 'src/app/Models/constants';
+import { AuthService } from 'src/app/Services/auth.service';
+import { BlogService } from 'src/app/Services/blog.service';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss'],
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit, OnDestroy {
+  isLoggedIn: boolean = false;
+  lastBlogs: Blog = <Blog>{};
+  dummyUserImage!: string;
+  blogObserver!: Subscription;
+  loginObserver!: Subscription;
 
+  constructor(
+    private blogService: BlogService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.loginObserver = this.authService.loggerObserver.subscribe((value) => {
+      this.dummyUserImage = value ? DUMMY_USER_IMAGE_LIGHT : DUMMY_USER_IMAGE;
+      this.isLoggedIn = value;
+    });
+
+    this.blogObserver = this.blogService.blogSubject.subscribe(
+      (blogs) => (this.lastBlogs = blogs[0])
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.loginObserver.unsubscribe();
+    this.blogObserver.unsubscribe();
+  }
 }
