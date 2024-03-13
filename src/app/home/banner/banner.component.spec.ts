d38aa7cf229d5b97c8b209c41b76a3954c9c0a2a
Irtiza Chowdhury <153156568+ich-wtag@users.noreply.github.com>
2024-03-13 15:07:30 +0600

    feat & style: home page banner created and styled for both login and logout condition

---
+++ src/app/home/banner/banner.component.spec.ts
@@ -0,0 +1,20 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BannerComponent } from './banner.component';
+
+describe('BannerComponent', () => {
+  let component: BannerComponent;
+  let fixture: ComponentFixture<BannerComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [BannerComponent],
+    });
+    fixture = TestBed.createComponent(BannerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
