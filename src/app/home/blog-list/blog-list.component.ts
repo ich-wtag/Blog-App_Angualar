ae3ec37eb85f4edca8f9726d5a162a5e82c0f921
Irtiza Chowdhury <153156568+ich-wtag@users.noreply.github.com>
2024-03-13 16:56:27 +0600

    feat & style: blog list created and styled

--- src/app/home/blog-list/blog-list.component.ts
+++ src/app/home/blog-list/blog-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Blog } from 'src/app/Models/blog';
+import { BlogService } from 'src/app/Services/blog.service';
 
 @Component({
   selector: 'app-blog-list',
   templateUrl: './blog-list.component.html',
-  styleUrls: ['./blog-list.component.scss']
+  styleUrls: ['./blog-list.component.scss'],
 })
-export class BlogListComponent {
+export class BlogListComponent implements OnInit, OnDestroy {
+  blogs: Blog[] = [];
+  blogObserverVer!: Subscription;
+  constructor(private blogService: BlogService) {}
 
+  ngOnInit(): void {
+    this.blogObserverVer = this.blogService.blogSubject.subscribe(
+      (data) => (this.blogs = data)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.blogObserverVer.unsubscribe();
+  }
 }
